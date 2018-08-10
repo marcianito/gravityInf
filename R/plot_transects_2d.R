e8ee6655761899e8e544d16457f7e9d873c6b338
Marvin Reich <mreich@gfz-potsdam.de>
2018-08-10 14:36:52 +0200

    modified transect plotting: now possible to automatically get N slices (in y direction) of SM model result profiles

--- R/plot_transects_2d.R
+++ R/plot_transects_2d.R
@@ -20,6 +20,7 @@ plot_transects_2d = function(
             soilmoisture_mod,
             plot_int,
             y_pos,
+            n_profiles = NA,
             vert_limit = NA,
             # input_dir = dir_input,
             output_dir = dir_output,
@@ -29,6 +30,11 @@ plot_transects_2d = function(
     # output_dir = dir_output
     # plot_int = plot_interval
     # y_pos = SG_y
+    # output_dir = "/home/mreich/Dokumente/written/beregnungsPaper/data/output/irrigation/plots/model_profiles/"
+    # plot_int = 60
+    # y_pos = -7
+    # n_profiles = 10
+    # soilmoisture_mod = "Infiltration_model_output_node161_1_2018-08-08_1502.rData"
 
     ## load modeled gravity response
     sm_mod_data = load(file = paste0(output_dir, soilmoisture_mod))
@@ -36,7 +42,8 @@ plot_transects_2d = function(
 
     # limit data to desired plotting time steps
     plot_ts = data.frame(
-                datetime = seq(sm_mod_data$datetime[1], sm_mod_data$datetime[length(sm_mod_data$datetime)], by = plot_int)
+                # datetime = seq(sm_mod_data$datetime[1], sm_mod_data$datetime[length(sm_mod_data$datetime)], by = plot_int)
+                datetime = seq(plot_int, sm_mod_data$datetime[length(sm_mod_data$datetime)], by = plot_int)
     )
     data_plot = dplyr::inner_join(plot_ts, sm_mod_data)
     
@@ -45,7 +52,47 @@ plot_transects_2d = function(
     data_plot = dplyr::filter(data_plot, Depth >= vert_limit)
     }
 
-    # limit data to one y profile
+if(!is.na(n_profiles)){
+    num_tot_profiles = length(unique(data_plot$y))
+    num_dis_profiles = trunc(num_tot_profiles / n_profiles)
+    select_profiles = seq(1,num_tot_profiles, by = num_dis_profiles)
+    select_ypos = unique(data_plot$y)[select_profiles]
+    
+    for(y_pos in select_ypos){
+        # limit data to one y profile
+        print(y_pos)
+        data_plot_pos = dplyr::filter(data_plot, y == y_pos)
+    
+        # get data value ranges
+        # data_min = min(data_plot$value, na.rm = T)
+        # data_max = max(data_plot$value, na.rm = T)
+        # data_mean = mean(data_plot$value, na.rm = T)
+        # data_qt = round(stats::quantile(data_plot$value * 100, na.rm = T, names = F), 2)
+        data_qt = trunc(c(min(data_plot$value * 100), max(data_plot$value * 100)))
+        
+        transect_2d.gg = ggplot(data_plot_pos, aes(x=x, y=Depth)) +
+            geom_tile(aes(fill = value *100)) + #, width = .25) + 
+            scale_fill_gradientn(breaks = data_qt, colours=rev(viridis(7)), na.value="red",
+                                 limits = c(min(data_plot$value * 100), max(data_plot$value * 100))) +
+            facet_grid(datetime ~ ., scale="free") +
+            ylab("Depth [m]") + xlab("Profile x [m]") + 
+            labs(fill = expression(Delta * "Soil moisture [%VWC]")) +
+            theme(legend.position ="bottom",
+        	      legend.text=element_text(size=15),
+        	      legend.title=element_text(size=15),
+        	      panel.grid.major = element_line(colour = "black", linetype = "dotted"),
+        	      panel.grid.minor = element_line(colour = "black", linetype = "dotted"))
+            
+        # save plot
+        png(filename = paste0(output_dir, "Modeled_2d_transect_ypos", y_pos, ".png"),
+                          width = 600,
+                          height = 100 * length(plot_ts$datetime),
+                          res = 150)
+        print(transect_2d.gg)
+        dev.off()
+}
+
+}else{
     data_plot = dplyr::filter(data_plot, y == y_pos)
 
     # get data value ranges
@@ -67,13 +114,14 @@ plot_transects_2d = function(
     	      panel.grid.minor = element_line(colour = "black", linetype = "dotted"))
         
     # save plot
-    png(filename = paste0(output_dir, "Modeled_2d_transect.png"),
+    png(filename = paste0(output_dir, "Modeled_2d_transect_ypos", y_pos, ".png"),
                       width = 600,
                       height = 100 * length(plot_ts$datetime),
                       res = 150)
     print(transect_2d.gg)
     dev.off()
-    
+}
+
     return(transect_2d.gg)
 }
 
